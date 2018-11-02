2181124b2091478965c84c36ec3b2933967a9bdd
ATT254017 <254017@via.dk>
2018-11-02 05:34:16 +0100

    Home page layout update

--- VIACinema/Pages/Index.cshtml.cs
+++ VIACinema/Pages/Index.cshtml.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using VIACinema.Data.Entities;
 
 namespace VIACinema.Pages
 {
     public class IndexModel : PageModel
     {
+
         public void OnGet()
         {
 
--- VIACinema/Startup.cs
+++ VIACinema/Startup.cs
@@ -68,7 +68,12 @@ namespace VIACinema
 
             app.UseAuthentication();
 
-            app.UseMvc();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
